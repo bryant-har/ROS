ddbde413eda441801354117fad1a3c9231a6bb7b
Bryant Har <bjh254@cornell.edu>
2022-10-03 17:32:53 -0400

    Vectorize compute_changes

--- src/cs4750_student/hw2_kinematics/car_kinematics/src/car_kinematics/kinematic_model.py
+++ src/cs4750_student/hw2_kinematics/car_kinematics/src/car_kinematics/kinematic_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from __future__ import division
-from cmath import cos, sin, tan
+from cmath import cos
 from threading import Lock
 import numpy as np
 from numpy.core.numeric import roll
@@ -68,23 +68,19 @@ class KinematicCarMotionModel:
 
         """
         # BEGIN "QUESTION 1.2" ALT="return np.zeros_like(states, dtype=float)"
-        data = np.hstack((states, controls))
+        res = np.zeros(states.shape)
 
-        def process(row):
-            if row[4] < alpha_threshold:
-                dx = row[3]*cos(row[2])*dt
-                dy = row[3]*sin(row[2])*dt
-                dtheta = 0
-            else:
-                dx = (self.car_length/tan(row[4])) * \
-                    (sin(row[2]+dtheta)-sin(row[2]))
-                dy = (self.car_length/tan(row[4])) * \
-                    (cos(row[2])-cos(row[2]+dtheta))
-                dtheta = (row[3]/self.car_length)*dt*tan(row[4])
-            return np.array([dx, dy, dtheta])
-
-        return np.apply_along_axis(process, 1, data)
-        # END
+        if controls[1] < alpha_threshold:
+            res[:, 0] = np.dot(controls[:, 0], dt*np.cos(states[:, 2]))
+            res[:, 1] = np.dot(controls[:, 0], dt*np.sin(states[:, 2]))
+        else:
+            res[:, 2] = np.dot(controls[0]/self.car_length,
+                               dt*np.tan(controls[1]))
+            res[:, 0] = np.dot((self.car_length)/np.tan(controls[1]),
+                               (np.sin(states[:, 2]+res[:, 2])-np.sin(states[:, 2])))
+            res[:, 1] = np.dot((self.car_length)/np.tan(controls[1]),
+                               (np.cos(states[:, 2])-np.cos(states[:, 2]+res[:, 2])))
+        return res        # END
 
     def apply_deterministic_motion_model(self, states, vel, alpha, dt):
         """Propagate states through the determistic kinematic car motion model.
