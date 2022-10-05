8d2b19102f91e34d56cdf27cd6b781fe757f38f1
Bryant Har <bjh254@cornell.edu>
2022-10-04 23:34:22 -0400

    j

--- src/cs4750_student/hw2_kinematics/arm_kinematics/src/arm_kinematics/fk_broadcaster.py
+++ src/cs4750_student/hw2_kinematics/arm_kinematics/src/arm_kinematics/fk_broadcaster.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from operator import matmul
 import rospy
 import tf2_ros
 import tf_conversions
@@ -8,54 +9,93 @@ from sensor_msgs.msg import JointState
 from tf.transformations import quaternion_from_matrix, translation_from_matrix
 import geometry_msgs.msg
 
+
 class Foward_Kinematics_Broadcaster:
 
     def __init__(self):
         rospy.loginfo("Initialize Joint State subscriber")
-        self.subscriber = rospy.Subscriber('/wx250s/joint_states', JointState, self.callback)
+        self.subscriber = rospy.Subscriber(
+            '/wx250s/joint_states', JointState, self.callback)
 
     def compute_fk(self, q_waist, q_shoulder, q_elbow):
-
         '''Feel free to comment out the following once you compute your own base_E_gripper'''
-        base_E_shoulder = np.matrix([[1,0,0,0.2],
-                                    [0,1,0,0.2],
-                                    [0,0,1,0.2],
-                                    [0,0,0,1]])
-        shoulder_E_Uarm = np.matrix([[1,0,0,0.2],
-                                    [0,1,0,0.2],
-                                    [0,0,1,0.2],
-                                    [0,0,0,1]])
-        Uarm_E_Uforearm = np.matrix([[1,0,0,0.2],
-                                    [0,1,0,0.2],
-                                    [0,0,1,0.2],
-                                    [0,0,0,1]])
-        Uforearm_E_gripper = np.matrix([[1,0,0,0.2],
-                                    [0,1,0,0.2],
-                                    [0,0,1,0.2],
-                                    [0,0,0,1]])
+        base_E_shoulder = np.matrix([[1, 0, 0, 0.2],
+                                    [0, 1, 0, 0.2],
+                                    [0, 0, 1, 0.2],
+                                    [0, 0, 0, 1]])
+        shoulder_E_Uarm = np.matrix([[1, 0, 0, 0.2],
+                                    [0, 1, 0, 0.2],
+                                    [0, 0, 1, 0.2],
+                                    [0, 0, 0, 1]])
+        Uarm_E_Uforearm = np.matrix([[1, 0, 0, 0.2],
+                                    [0, 1, 0, 0.2],
+                                    [0, 0, 1, 0.2],
+                                    [0, 0, 0, 1]])
+        Uforearm_E_gripper = np.matrix([[1, 0, 0, 0.2],
+                                        [0, 1, 0, 0.2],
+                                        [0, 0, 1, 0.2],
+                                        [0, 0, 0, 1]])
         '''
-        Input: 
+        Input:
             q_waist: value for waist joint, the first slider on joint_state_publisher_gui controls this
             q_shoulder: value for shoulder joint
             q_elbow: value for elbow joint
-        Return: 
+        Return:
             base_E_shoulder:     An numpy matrix (4x4) that represent the forward kinematics from base_link to shoulder_link
             shoulder_E_Uarm:     An numpy matrix (4x4) that represent the forward kinematics from shoulder_link to upper_arm_link
             Uarm_E_Uforearm:     An numpy matrix (4x4) that represent the forward kinematics from upper_arm_link to upper_forearm_link
             Uforearm_E_gripper:  An numpy matrix (4x4) that represent the forward kinmeatics from upper_forearm_link to ee_gripper_link
 
-        Hint: 
+        Hint:
             + Define a helper function that computes the transformation matrix from frame {i-1} to frame {i} given the 4 D-H parameters:
                 i.e. define a function dh_transform(self, alpha, a, d, phi) that returns the transformation matrix matrix (numpy matrix (4x4))
             + Feel free to use np.sin() and np.cos().
         '''
         '''Compute the forward kinematics transformation matrices for Widowx250'''
 
-        
         # BEGIN SOLUTION 2.2
 
-        # END SOLUTION 
-        
+        def dh_transform(self, alpha, a, d, phi):
+            alphamatrix = np.array([
+                [1, 0, 0, 0],
+                [0, np.cos(alpha), -np.sin(alpha), 0],
+                [0, np.sin(alpha), np.cos(alpha), 0],
+                [0, 0, 0, 1]
+            ])
+
+            amatrix = np.array([
+                [1, 0, 0, a]
+                [0, 1, 0, 0],
+                [0, 0, 1, 0],
+                [0, 0, 0, 1]
+
+            ])
+
+            dmatrix = np.array([
+                [1, 0, 0, 0],
+                [0, 1, 0, 0],
+                [0, 0, 1, d],
+                [0, 0, 0, 1]
+            ])
+
+            phimatrix = np.array([
+                [np.cos(phi), -np.sin(phi), 0, 0],
+                [np.sin(phi), np.cos(phi), 0, 0],
+                [0, 0, 1, 0],
+                [0, 0, 0, 1]
+            ])
+
+            res1 = np.matmul(alphamatrix, amatrix)
+            res2 = np.matmul(res1, dmatrix)
+            return np.matmul(res2, phimatrix)
+
+        base_E_shoulder = dh_transform()
+        shoulder_E_Uarm = dh_transform
+        Uarm_E_Uforearm = dh_transform
+        Uforearm_E_gripper = dh_transform
+
+        # END SOLUTION
+
         return base_E_shoulder, shoulder_E_Uarm, Uarm_E_Uforearm, Uforearm_E_gripper
 
     def callback(self, msg):
@@ -64,9 +104,10 @@ class Foward_Kinematics_Broadcaster:
         q_shoulder = joints_angle[1]
         q_elbow = joints_angle[2]
 
-        base_E_shoulder, shoulder_E_Uarm, Uarm_E_Uforearm, Uforearm_E_gripper = self.compute_fk(q_waist, q_shoulder, q_elbow)
-        base_E_gripper = base_E_shoulder * shoulder_E_Uarm * Uarm_E_Uforearm * Uforearm_E_gripper
-
+        base_E_shoulder, shoulder_E_Uarm, Uarm_E_Uforearm, Uforearm_E_gripper = self.compute_fk(
+            q_waist, q_shoulder, q_elbow)
+        base_E_gripper = base_E_shoulder * shoulder_E_Uarm * \
+            Uarm_E_Uforearm * Uforearm_E_gripper
 
         q = quaternion_from_matrix(base_E_gripper)
         translation = translation_from_matrix(base_E_gripper)
@@ -86,6 +127,3 @@ class Foward_Kinematics_Broadcaster:
         t.transform.rotation.w = q[3]
 
         br.sendTransform(t)
-
-        
-
