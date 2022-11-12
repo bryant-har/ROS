8363a818b47fc241451fc1d5729bcbf10c52d8c7
bjh254 <bjh254@cornell.edu>
2022-11-11 22:53:00 -0500

    few

--- src/cs4750_student/hw4_planning/planning/src/planning/search.py
+++ src/cs4750_student/hw4_planning/planning/src/planning/search.py
@@ -105,6 +105,7 @@ class ASTARPlanner(object):
         """
         # Follow the parents of the node until a NULL entry is reached
         ### BEGIN QUESTION 1.2 #####################
+        print("FEW")
         path = [goal]
         curr = goal
         while parents[curr] is not NULL:
